c464332e4a5290a48584c50953355f43591505ba
Shaolong Chen <35615289+EricChen9581@users.noreply.github.com>
2018-10-12 14:35:13 -0500

    2sum
    HashMap解法

--- N Sum/2 Sum.java
+++ N Sum/2 Sum.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        if(nums == null || nums.length <= 1)
+            return res;
+        
+        for(int i = 0; i < nums.length; i ++) {
+            int tmp = target - nums[i];
+            if(map.containsKey(tmp)) {
+                res[1] = i;
+                res[0] = map.get(tmp);
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+      return new int[]{-1, -1};
+    }
+}
