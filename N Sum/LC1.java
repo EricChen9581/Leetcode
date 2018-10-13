64acc176e511118d1109e12838e8e86afc3f079e
Shaolong Chen <35615289+EricChen9581@users.noreply.github.com>
2018-10-13 11:00:08 -0500

    总结几种经典思想

--- N Sum/LC1.java
+++ N Sum/LC1.java
@@ -1,5 +1,9 @@
 class Solution {
     // 回顾经典题
+    // 1. N^2 解法, brute force
+    // 2. Use hashmap to store the previous data O(N)
+    // 3. sort and use binary searh O(NlgN)
+    // 4. sort and use 2 pointers 0(NlgN)
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
