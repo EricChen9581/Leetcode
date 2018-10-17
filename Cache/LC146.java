6f15e9b02c5447ad32f89a3ace3f19e18c8f521f
Shaolong Chen <35615289+EricChen9581@users.noreply.github.com>
2018-10-16 19:45:11 -0500

    Update LC146.java

--- Cache/LC146.java
+++ Cache/LC146.java
@@ -1,1 +1,7 @@
-// 
+// HashMap + Double LinkedList
+// LRU : Least Recently Used cache.
+// It has 2 methods, the first is get, the other is put.
+// Get: get the value of the key, if didn't exist, return -1;
+// and after using, put the recently used pair into the first place.
+// Put: Put the new key-value pair into the queue. if reached the capacity, firstly we need to drop the last element and add the new element.
+//
